f1036560354226719ae2062f86a5b76932163e17
ChrisKeijzer02 <117999606+ChrisKeijzer02@users.noreply.github.com>
2022-11-18 14:03:58 +0100

    Completing excercise 6.12
    creating subcommand functions for the sine and the tangent. For both you have the option to choose the number of steps

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,20 +3,43 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+# Making command group
+@click.group()
+def cmd_group():
+    pass
 
-def sin(number):
-    x = np.linspace(0, 2 * pi, number)
+# creating subcommand with optional amount of steps
+@cmd_group.command()
+@click.option(
+"-c",
+"--count",
+default=10,
+help="Number of times to print greeting.",
+show_default=True, # show default in help
+)
+# Creating sine function
+def sin(count): 
+    x = np.linspace(0, 2 * pi, count)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
-
-def tan(number):
-    x = np.linspace(0, 2 * pi, number)
+# creating subcommand with optional amount of steps
+@cmd_group.command()
+@click.option(
+"-c",
+"--count",
+default=10,
+help="Number of times to print greeting.",
+show_default=True, # show default in help
+)
+# Creating tan function
+def tan(count):
+    x = np.linspace(0, 2 * pi, count)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
