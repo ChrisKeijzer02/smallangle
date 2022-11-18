31f17c35648587d326fe6184c6009685bfe68a10
ChrisKeijzer02 <117999606+ChrisKeijzer02@users.noreply.github.com>
2022-11-18 15:01:48 +0100

    Adding docstring

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -11,7 +11,7 @@ def cmd_group():
 # creating subcommand with optional amount of steps
 @cmd_group.command()
 @click.option(
-"-n",
+"-c",
 "--count",
 default=10,
 help="Number of times to print greeting.",
@@ -19,9 +19,15 @@ show_default=True, # show default in help
 )
 # Creating sine function
 def sin(count): 
+    """Making a sine function that runs from 0 till 2pi. Then making a dataframe of the input and output and printing that.
+
+    Args:
+        count (string): number of steps between 0 and 2pi
+    """
     x = np.linspace(0, 2 * pi, count)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
+
     return
 
 # creating subcommand with optional amount of steps
@@ -35,6 +41,11 @@ show_default=True, # show default in help
 )
 # Creating tan function
 def tan(count):
+    """Making a tangent function that runs from 0 till 2pi. Then making a dataframe of the input and output and printing that.
+
+    Args:
+        count (string): number of steps between 0 and 2pi
+    """    
     x = np.linspace(0, 2 * pi, count)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
